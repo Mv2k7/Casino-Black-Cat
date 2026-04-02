0b832c6b4ed6a2b7daa185f8c21be9d1354f4215
Marcelo <m.vega10@ufromail.cl>
2026-04-01 22:42:28 -0300

    Último commit antes de subir el final, se terminó todos los métodos y cada uno cumple su función, ahora se verá como mejorar algunas partes del código y hacer que sea repetitivo hasta la opción de salir

--- src/Ruleta.java
+++ src/Ruleta.java
@@ -37,58 +37,126 @@ public class Ruleta {
     public static int leerOpcion(Scanner in) {
         System.out.println("Selecciona tu opción: ");
         int opcion = in.nextInt();
-        return opcion;
+        return opcion; // creo que puede ser mas corto
     }
 
     public static void ejecutarOpcion(int opcion, Scanner in) {
         if (opcion == 1) {
             System.out.println("Seleccionaste la opcion: Iniciar una ronda\n");
-            leerOpcion(in);
+            iniciarRonda(in);
         } else if (opcion == 2) {
             System.out.println("Seleccionaste la opcion: Ver Estadísticas\n");
         } else if (opcion == 3) {
             System.out.println("Seleccionaste la opcion: Salir\n");
         } else {
-            System.out.println("Opción inválida...");}
+            System.out.println("Opción inválida...");
+        }
     }
 
     public static char leerTipoApuesta(Scanner in) {
         System.out.println("BIENVENIDO A LA RULETA!" +
                 "\nSelecciona tu tipo de apuesta" +
-                "\nP/I (Par o Impar) Y R/N (Rojo o Negro)" +
-                "\nLuego, elige el monto a jugar.");
+                "\nP/I (Par o Impar) Y R/N (Rojo o Negro)");
 
         char apuesta = in.next().charAt(0); // Para la primera letra
+        apuesta = Character.toLowerCase(apuesta); // Para que sea minúscula
 
-        while (apuesta != 'P' && apuesta != 'I' && apuesta != 'R' && apuesta != 'N') {
+        while (apuesta != 'p' && apuesta != 'i' && apuesta != 'r' && apuesta != 'n') {
             System.out.println("Opcion inválida, intenta otra vez por favor.");
             apuesta = in.next().charAt(0);
+            // Esto es para que si elige cualquier valor, que no sea p/i/r/n, reintente.
         }
 
         return apuesta;
     }
 
     public static void iniciarRonda(Scanner in) {
+        // Aquí voy a definir todas las variables utilizadas al momento de jugar
+        char tipo = leerTipoApuesta(in);
+        System.out.println("ELIGE EL MONTO");
+        int monto = in.nextInt(); // monto a jugar
+        System.out.println("ELIGE EL NÚMERO!!");
+        int numero = girarRuleta();
+        boolean acierto = evaluarResultado(numero, tipo);
 
+        registrarResultado(numero, monto, acierto);
+        mostrarResultado(numero, tipo, monto, acierto);
     }
 
     public static int girarRuleta() {
-        return 0;
+        return rng.nextInt(37); // retorna un número random desde el 0 hasta el 36
     }
 
     public static boolean evaluarResultado(int numero, char tipo) {
-
+        switch (tipo) {
+            case 'r':
+                return esRojo(numero);
+            case 'n':
+                return !esRojo(numero) && numero != 0;
+            case 'p':
+                return numero % 2 == 0 && numero != 0;
+            case 'i':
+                return numero % 2 != 0;
+        }
         return false;
     }
 
     public static boolean esRojo(int n) {
-        return false;
+        for (int i = 0; i < numerosRojos.length; i++) {
+            if (numerosRojos[i] == n) {
+                return true;
+            }
+        }
+        return false; // como no está en la lista, retornará false.
     }
 
-    public static void registrarResultado(int numero, int apuesta, boolean acierto) {}
+    public static void registrarResultado(int numero, int apuesta, boolean acierto) {
+        historialNumeros[historialSize] = numero;
+        historialApuestas[historialSize] = apuesta;
+        historialAciertos[historialSize] = acierto;
+        historialSize++; // para que no se sobreescriba
+    }
 
     public static void mostrarResultado(int numero, char tipo, int monto, boolean
-            acierto) {}
+            acierto) {
+        if (tipo == 'r') System.out.println("Seleccionaste: color Rojo");
+        if (tipo == 'n') System.out.println("Seleccionaste: color Negro");
+        if (tipo == 'p') System.out.println("Seleccionaste: números Par");
+        if (tipo == 'i') System.out.println("Seleccionaste: números Impar");
+        System.out.println("MONTO EN APUESTA: " + monto);
+        System.out.println("SALIÓ EL NÚMERO: " + numero);
+        if (acierto) {
+            System.out.println("FELICIDADES!!! HAS GANADO!");
+        } else {
+            System.out.println("Perdiste esta vez...");
+        }
+    }
 
-    public static void mostrarEstadisticas() {}
+    public static void mostrarEstadisticas() {
+        int partidas = historialSize;
+        int gastado = 0;
+        int aciertos = 0;
+        int ganancias = 0;
+
+        for (int i = 0; i < historialSize; i++) {
+            gastado += historialApuestas[i];
+
+            if (historialAciertos[i]) {
+                aciertos++;
+                ganancias += historialApuestas[i];
+            } else {
+                ganancias -= historialApuestas[i];
+            }
+        }
+        double porcentaje = 0;
+        // para que no divida por 0 y sea positivo, tiene que cumplir que las partidas sean mayores que 0.
+        if (partidas > 0) {
+            porcentaje = (double) aciertos / partidas * 100;
+        }
+        System.out.println("Partidas jugadas: " + partidas +
+                "\nTotal apostado: " + gastado +
+                "\nTotal aciertos: " + aciertos +
+                "\nPorcentaje de aciertos " + porcentaje + "%" +
+                "\nGanancia o Pérdida: " + ganancias);
+    }
 }
